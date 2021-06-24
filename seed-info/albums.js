adfc5f8bbde12011ada2846fdf35f2e425053a3d
Monte <36746765+theflaggship@users.noreply.github.com>
2021-06-24 09:34:57 -0700

    added genres in seed-info

--- seed-info/albums.js
+++ seed-info/albums.js
@@ -1,1 +1,55 @@
 // {"Title", "Artist", "Songs"??, Genre_Id, Owner_Count}
+// { title: "Title",  artist: "Artist", genreId: #, ownerCount: 0, createdAt: new Date(), updatedAt: new Date()},
+
+
+//* Monte's
+
+//1.
+//2.
+//3.
+//4.
+//5.
+//6.
+//7.
+//8.
+//9.
+//10.
+
+//* Brad's
+
+//1.
+//2.
+//3.
+//4.
+//5.
+//6.
+//7.
+//8.
+//9.
+//10.
+
+//* Christian's
+
+//1.
+//2.
+//3.
+//4.
+//5.
+//6.
+//7.
+//8.
+//9.
+//10.
+
+//* Owen's
+
+//1.
+//2.
+//3.
+//4.
+//5.
+//6.
+//7.
+//8.
+//9.
+//10.
---
+++ seed-info/genres.js
@@ -0,0 +1,12 @@
+// {name: "Name", createdAt: new Date(), updatedAt: new Date()}
+
+// { name: "Hip-Hop", createdAt: new Date(), updatedAt: new Date() }
+// { name: "R&B", createdAt: new Date(), updatedAt: new Date() }
+// { name: "Alternative/Indie", createdAt: new Date(), updatedAt: new Date() }
+// { name: "Country", createdAt: new Date(), updatedAt: new Date() }
+// { name: "Classic Rock", createdAt: new Date(), updatedAt: new Date() }
+// { name: "Jazz", createdAt: new Date(), updatedAt: new Date() }
+// { name: "Pop", createdAt: new Date(), updatedAt: new Date() }
+// { name: "Electronic", createdAt: new Date(), updatedAt: new Date() }
+// { name: "Rock", createdAt: new Date(), updatedAt: new Date() }
+// { name: "Reggae", createdAt: new Date(), updatedAt: new Date() }
