82c083590bc1e666c3fae0df0550b249b36b3321
Owen Iwamasa <owiwamasa@gmail.com>
2021-06-24 20:40:50 -0400

    added owens albums

--- seed-info/albums.js
+++ seed-info/albums.js
@@ -42,13 +42,13 @@
 
 //* Owen's
 
-//1.
-//2.
-//3.
-//4.
-//5.
-//6.
-//7.
-//8.
-//9.
-//10.
+//1.{ title: "Invasion of Privacy",  artist: "Cardi B", genreId: #, ownerCount: 0, releaseDate: 2018, imgSrc: "https://upload.wikimedia.org/wikipedia/en/9/97/Cardi_B_-_Invasion_of_Privacy.png", createdAt: new Date(), updatedAt: new Date()},
+//2.{ title: "Operation: Doomsday",  artist: "MF DOOM", genreId: #, ownerCount: 0, releaseDate: 1999, imgSrc: "https://upload.wikimedia.org/wikipedia/en/0/03/MF-DOOM-Operation.jpeg", createdAt: new Date(), updatedAt: new Date()},
+//3.{ title: "Ready To Die",  artist: "The Notorious B.I.G.", genreId: #, ownerCount: 0, releaseDate: 1994, imgSrc: "https://upload.wikimedia.org/wikipedia/en/9/97/Ready_To_Die.jpg", createdAt: new Date(), updatedAt: new Date()},
+//4.{ title: "Troupeau Bleu",  artist: "Cortex", genreId: #, ownerCount: 0, releaseDate: 1975, imgSrc: "https://f4.bcbits.com/img/a1112421945_16.jpg", createdAt: new Date(), updatedAt: new Date()},
+//5.{ title: "A Love Supreme",  artist: "John Coltrane", genreId: #, ownerCount: 0, releaseDate: 1965, imgSrc: "https://upload.wikimedia.org/wikipedia/en/9/9a/John_Coltrane_-_A_Love_Supreme.jpg", createdAt: new Date(), updatedAt: new Date()},
+//6.{ title: "Choose Your Weapon",  artist: "Hiatus Kaiyote", genreId: #, ownerCount: 0, releaseDate: 2015, imgSrc: "https://upload.wikimedia.org/wikipedia/en/e/e9/HKaiyote_Weapon.jpg", createdAt: new Date(), updatedAt: new Date()},
+//7.{ title: "I Want You",  artist: "Marvin Gaye", genreId: #, ownerCount: 0, releaseDate: 1976, imgSrc: "https://upload.wikimedia.org/wikipedia/en/e/e3/Marvin-gaye-i-want-you.jpg", createdAt: new Date(), updatedAt: new Date()},
+//8.{ title: "That's The Way of The World",  artist: "Earth Wind & Fire", genreId: #, ownerCount: 0, releaseDate: 1975, imgSrc: "https://upload.wikimedia.org/wikipedia/en/0/03/Whiskeytown-Stranger%27s_Almanac_%28album_cover%29.jpg", createdAt: new Date(), updatedAt: new Date()},
+//9.{ title: "Worldwide Underground",  artist: "Erykah Badu", genreId: #, ownerCount: 0, releaseDate: 2003, imgSrc: "https://upload.wikimedia.org/wikipedia/en/a/a4/Erykah_Badu_-_Wordwide_Underground.jpg", createdAt: new Date(), updatedAt: new Date()},
+//10.{ title: "What You Won't Do For Love",  artist: "Bobby Caldwell", genreId: #, ownerCount: 0, releaseDate: 1978, imgSrc: "https://upload.wikimedia.org/wikipedia/en/8/8a/Bobby_Caldwell_%28album%29.jpg", createdAt: new Date(), updatedAt: new Date()},
