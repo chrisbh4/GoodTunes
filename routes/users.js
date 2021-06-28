79b44fe97f2f69c0c8434cf2e02d1fa4676a5fb9
Owen Iwamasa <owiwamasa@gmail.com>
2021-06-28 18:03:06 -0400

    added sign up page

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,69 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs')
+const db = require('../db/models')
+const { csrfProtection, asyncHandler } = require('./utils');
+const { check, validationResult } = require('express-validator');
+// const { check } = require('yargs');
+const { User } = db
 
+const hashedPassword = async (password, salt) => {
+  const hash = await bcrypt.hash(password, salt)
+  return hash
+}
+
+const userValidators = [
+  check('username')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for username')
+    .isLength({ max: 30 })
+    .withMessage('Username must not be more than 30 characters long'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for password')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
+    .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
+  check('confirmPassword')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for confirm password')
+    .isLength({ max: 15 })
+    .withMessage('Confirm Password must not be more than 50 characters long')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Confirm Password does not match Password');
+      }
+      return true;
+    }),
+]
+
+router.get('/signup', csrfProtection, (req, res) => {
+  res.render('sign-up', { csrfToken: req.csrfToken() })
+})
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/signup', csrfProtection, userValidators, asyncHandler(async (req, res, next) => {
+  const { username, password } = req.body
+
+  const hashedPw = hashedPassword(password, 10)
+
+  const user = User.build({ username })
+
+  const validatorErrors = validationResult(req)
+
+  if (validatorErrors.isEmpty()) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user.hashedPassword = hashedPassword;
+    await user.save();
+    //TODO loginUser
+    res.redirect('/users/signup')
+  } else {
+    const errors = validatorErrors.array().map((error) => error.msg);
+    res.render('user-register', {
+      title: 'Register',
+      user,
+      errors,         //TODO display error messages in pug file
+      csrfToken: req.csrfToken(),
+    })
+  }
+}));
 
 module.exports = router;
---
+++ routes/utils.js
@@ -0,0 +1,18 @@
+const csrf = require('csurf')
+const csrfProtection = csrf({ cookie: true })
+const cookieParser = require('cookie-parser')
+
+const asyncHandler = (handler) => {
+    return (req, res, next) => {
+        return handler(req, res, next).catch(next)
+    }
+}
+
+
+
+
+
+
+
+
+module.exports = { csrfProtection, asyncHandler }
