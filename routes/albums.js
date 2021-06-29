6257d2150351f54149dd3ed10b8a640c5e9e9daf
brad <bradsherwood24816@gmail.com>
2021-06-29 10:17:07 -0700

    albums routes

---
+++ routes/albums.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db/models');
+const { csrfProtection, asyncHandler } = require('./utils');
+const { User, Review, Album, Song, Genre } = db
+const {  requireAuth} = require('../auth')
+
+router.get('/:id', asyncHandler, async(req, res) => {
+    const id = req.params.id
+    const album = await Album.findByPk(id, {
+        include: Genre
+    })
+    res.render('album', {album})
+})
+
+
+
+
+module.exports =  router;
