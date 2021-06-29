f5823bb5013bfa652922eb8d3cb27effe87c049b
Owen Iwamasa <owiwamasa@gmail.com>
2021-06-29 13:25:03 -0400

    solved albums page bug

--- routes/albums.js
+++ routes/albums.js
@@ -3,17 +3,21 @@ const router = express.Router();
 const db = require('../db/models');
 const { csrfProtection, asyncHandler } = require('./utils');
 const { User, Review, Album, Song, Genre } = db
-const {  requireAuth} = require('../auth')
+const { requireAuth } = require('../auth')
 
-router.get('/:id', asyncHandler, async(req, res) => {
+router.get('/', (req, res) => {
+    res.render('album')
+})
+
+router.get('/:id', asyncHandler(async (req, res) => {
     const id = req.params.id
     const album = await Album.findByPk(id, {
         include: Genre
     })
-    res.render('album', {album})
-})
+    res.render('album', { album })
+}))
 
 
 
 
-module.exports =  router;
+module.exports = router;
