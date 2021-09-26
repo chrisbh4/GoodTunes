cf687c5cbd96539e7038616ca294da262fb577f0
Owen Iwamasa <owiwamasa@gmail.com>
2021-09-26 13:21:59 -0700

    restyled album page

--- routes/albums.js
+++ routes/albums.js
@@ -32,10 +32,15 @@ router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
     var url = `https://api.discogs.com/masters/${id}?key=${process.env.DC_KEY}&secret=${process.env.DC_SECRET}`
     var response = await fetch(url)
     const album = await response.json()
+    console.log(album.tracklist[0])
 
-    //TODO: Add Reviews
-    // res.send(album)
-    res.render('album', { album })
+    const reviews = await Review.findAll({
+        where: {
+            albumId: album.id
+        },
+        include: User
+    })
+    res.render('album', { album, reviews })
 
 
 
