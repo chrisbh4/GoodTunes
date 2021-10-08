adf2d97cc9201179531f24333b3473b1fd4224ef
Owen Iwamasa <owiwamasa@gmail.com>
2021-10-07 17:35:48 -0700

    altering seed files

--- db/seeders/20210628175315-Genre-seeds.js
+++ db/seeders/20210628175315-Genre-seeds.js
@@ -10,16 +10,16 @@ module.exports = {
       */
    return queryInterface.bulkInsert('Genres', [
      { name: "Hip-Hop", createdAt: new Date(), updatedAt: new Date() },
-     { name: "R&B", createdAt: new Date(), updatedAt: new Date() },
-     { name: "Alternative/Indie", createdAt: new Date(), updatedAt: new Date() },
-     { name: "Country", createdAt: new Date(), updatedAt: new Date() },
-     { name: "Classic Rock", createdAt: new Date(), updatedAt: new Date() },
-     { name: "Jazz", createdAt: new Date(), updatedAt: new Date() },
-     { name: "Pop", createdAt: new Date(), updatedAt: new Date() },
-     { name: "Electronic", createdAt: new Date(), updatedAt: new Date() },
-     { name: "Rock", createdAt: new Date(), updatedAt: new Date() },
-     { name: "Reggae", createdAt: new Date(), updatedAt: new Date() },
-     { name: "Rap", createdAt: new Date(), updatedAt: new Date() }
+    //  { name: "R&B", createdAt: new Date(), updatedAt: new Date() },
+    //  { name: "Alternative/Indie", createdAt: new Date(), updatedAt: new Date() },
+    //  { name: "Country", createdAt: new Date(), updatedAt: new Date() },
+    //  { name: "Classic Rock", createdAt: new Date(), updatedAt: new Date() },
+    //  { name: "Jazz", createdAt: new Date(), updatedAt: new Date() },
+    //  { name: "Pop", createdAt: new Date(), updatedAt: new Date() },
+    //  { name: "Electronic", createdAt: new Date(), updatedAt: new Date() },
+    //  { name: "Rock", createdAt: new Date(), updatedAt: new Date() },
+    //  { name: "Reggae", createdAt: new Date(), updatedAt: new Date() },
+    //  { name: "Rap", createdAt: new Date(), updatedAt: new Date() }
    ], {});
   },
 
